06ecf0608826b68f07f51d4dd06cf7c7b1670621
Michael Douglas <michael-douglas.tech@outlook.com>
2025-04-09 19:01:30 -0300

    12

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ function adicionarCampoImagem() {
     const input = document.createElement('input');
     input.type = 'file';
     input.accept = 'image/*';
-    input.capture = 'environment';
+    // input.capture = 'environment';
 
     const img = document.createElement('img');
     img.style.display = 'none';
@@ -35,84 +35,6 @@ function adicionarCampoImagem() {
   }
 
   window.onload = adicionarCampoImagem;
-
-// //   cropper
-
-// let cropper; // Variável global para o Cropper
-// let imagemAtual; // Armazena o elemento img que será atualizado após o recorte
-
-// function adicionarCampoImagem() {
-//   const container = document.getElementById('fotos-container');
-
-//   const div = document.createElement('div');
-//   div.classList.add('foto-item');
-
-//   const input = document.createElement('input');
-//   input.type = 'file';
-//   input.accept = 'image/*';
-//   input.capture = 'environment';
-
-//   const img = document.createElement('img');
-//   img.style.display = 'none';
-
-//   const textarea = document.createElement('textarea');
-//   textarea.name = 'descricao_imagem[]';
-//   textarea.placeholder = 'Descreva a imagem...';
-
-//   input.addEventListener('change', function () {
-//     const file = this.files[0];
-//     if (file) {
-//       const reader = new FileReader();
-//       reader.onload = function (e) {
-//         const imagemCropper = document.getElementById('imagemCropper');
-//         imagemCropper.src = e.target.result;
-
-//         // Exibe o modal de crop
-//         document.getElementById('cropperModal').style.display = 'block';
-
-//         // Salva o img que será atualizado após o crop
-//         imagemAtual = img;
-
-//         // Destroi instância anterior (se houver)
-//         if (cropper) cropper.destroy();
-
-//         // Inicializa o Cropper
-//         cropper = new Cropper(imagemCropper, {
-//           aspectRatio: NaN,
-//           viewMode: 1,
-//         });
-//       };
-//       reader.readAsDataURL(file);
-//     }
-//   });
-
-//   div.appendChild(input);
-//   div.appendChild(img);
-//   div.appendChild(textarea);
-//   container.appendChild(div);
-// }
-
-// function recortarImagem() {
-//   if (cropper) {
-//     const canvas = cropper.getCroppedCanvas();
-//     const dataURL = canvas.toDataURL('image/jpeg');
-//     imagemAtual.src = dataURL;
-//     imagemAtual.style.display = 'block';
-
-//     // Fecha o modal
-//     fecharModal();
-//   }
-// }
-
-// function fecharModal() {
-//   document.getElementById('cropperModal').style.display = 'none';
-//   if (cropper) {
-//     cropper.destroy();
-//     cropper = null;
-//   }
-// }
-
-// window.onload = adicionarCampoImagem;
 
 
 //  ADD FOTOS
